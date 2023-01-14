a1af7122b082d54f205e1cf68f8a544a5dea9ac8
Adrian Bonel <adrianbonel93@gmail.com>
2023-01-14 01:58:04 +0100

    Memoria final

--- esp32/update_files.sh
+++ esp32/update_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ampy --port /dev/ttyUSB0 put main.py
-ampy --port /dev/ttyUSB0 put acs712_cfg.py
-ampy --port /dev/ttyUSB0 put mqtt_cfg.py
-ampy --port /dev/ttyUSB0 put zmpt101b.py
-ampy --port /dev/ttyUSB0 mkdir lib
-ampy --port /dev/ttyUSB0 put lib/umqttsimple.py lib/umqttsimple.py
-ampy --port /dev/ttyUSB0 put lib/ssd1306.py lib/ssd1306.py
+ampy --port /dev/ttyUSB1 put main.py
+ampy --port /dev/ttyUSB1 put acs712_cfg.py
+ampy --port /dev/ttyUSB1 put mqtt_cfg.py
+ampy --port /dev/ttyUSB1 put zmpt101b.py
+ampy --port /dev/ttyUSB1 mkdir lib
+ampy --port /dev/ttyUSB1 put lib/umqttsimple.py lib/umqttsimple.py
+ampy --port /dev/ttyUSB1 put lib/ssd1306.py lib/ssd1306.py
